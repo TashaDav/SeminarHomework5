102cce4ff24401a49a221f3145b581615ad76d10
davydova <natalya-davydova-86@mail.ru>
2023-05-29 14:41:00 +0300

    Created Task 036

---
+++ Task036/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайть одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] randomArr = new int[n];
+
+void mas(int n)
+{
+for (int i = 0; i < n; i++)
+{
+randomArr[i] = new Random().Next(1,20);
+Console.Write(randomArr[i] + " ");
+}
+
+}
+
+int element(int[] randomArr)
+{
+int sum = 0;
+int i = 1;
+while (i < randomArr.Length)
+{
+sum = sum + randomArr[i];
+i = i + 2;
+}
+return sum;
+}
+
+mas(n);
+Console.Write($"Cумма элементов на нечётных позициях =  {element(randomArr)}");
